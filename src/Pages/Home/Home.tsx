92820475f178910e30e8760b5a994a881a421e7c
ALI-RAZA75 <alirazaf75439@gmail.com>
2024-07-15 01:15:51 +0500

    Menu section done

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,8 +1,21 @@
+import Topbox from '../../Components/TopBox/Topbox'
 import './Home.scss'
 
 function Home() {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <div className="box box-1">
+        <Topbox/>
+      </div>
+      <div className="box box-2">box 2</div>
+      <div className="box box-3">box 3</div>
+      <div className="box box-4">box 4</div>
+      <div className="box box-5">box 5</div>
+      <div className="box box-6">box 6</div>
+      <div className="box box-7">box 7</div>
+      <div className="box box-8">box 8</div>
+      <div className="box box-9">box 9</div>
+    </div>
   )
 }
 
